3337a15a0ae855cb4f0e4b1dd060f31ba236cb95
siweh <sanemadesi@gmail.com>
2021-12-01 15:00:46 +0200

    Added the new model

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const exphbs  = require('express-handlebars');
+const exphbs = require('express-handlebars');
 const bodyParser = require('body-parser');
 const pg = require('pg');
 
@@ -12,18 +12,15 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(express.static('public'));
 
-app.engine('handlebars', exphbs.engine({ defaultLayout: 'main' }));
+app.engine('handlebars', exphbs.engine({ defaultLayout: false }));
 app.set('view engine', 'handlebars');
-   
-app.get('/', function(req, res) {
 
-	res.render('index', {
-
-	});
+app.get('/', function (req, res) {
+  res.render('index', {});
 });
 
 //start everything up
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, function () {
-    console.log('App started at PORT: ', PORT);
+  console.log('App started at PORT: ', PORT);
 });
